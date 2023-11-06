d7f39e0b8a0a9ab6e5d661273b36d4496ffbc6b2
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-06 18:41:09 -0500

    ✨ feat: Temporay home page

--- app.vue
+++ app.vue
@@ -1,13 +1,26 @@
-<template>
+<!-- <template>
   <div>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
-</template>
+</template> -->
 
 <script setup>
 const appConfig = useAppConfig();
-
-console.log(appConfig.ui);
+useSeoMeta({
+  title: "Brainstorm",
+  ogTitle: "Brainstorm",
+  // description: "This is my amazing site, let me tell you all about it.",
+  // ogDescription: "This is my amazing site, let me tell you all about it.",
+  // ogImage: "https://example.com/image.png",
+  // twitterCard: "summary_large_image",
+});
 </script>
+
+<template>
+  <h1 class="text-9xl font-bold m-12 text-white">Brainstorm .</h1>
+  <h2 class="text-7xl font-bold m-12 text-white">
+    Our website is coming soon ...
+  </h2>
+</template>
