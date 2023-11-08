d5df2c97a8f5f3bd620fee716ee497134edde092
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-08 00:10:08 -0500

    ✨ feat: Summary boxes

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,33 @@
-<template>
-  <h1>Index page</h1>
+<template class="flex items-center">
+  <h1 class="text-3xl">Index page</h1>
+  <div
+    class="summary w-1/3 mx-auto grid grid-cols-2 grid-rows-4 h-screen pt-10 gap-5"
+  >
+    <div
+      class="flex flex-col gap-8 justify-end row-start-1 row-end-3 bg-chart-blue-300 rounded-lg shadow-md p-4"
+    >
+      <img
+        src="../assets/svg/podcast.svg"
+        class="h-80"
+        alt="Description de l'icône SVG"
+      />
+      <p class="text-white text-3xl font-bold">Podcast récent</p>
+    </div>
+    <div class="bg-chart-pink-500 rounded-lg shadow-md p-4">
+      <p class="text-white text-lg">Coming soon ..</p>
+    </div>
+    <div class="bg-chart-brown-400 rounded-lg shadow-md p-4">
+      <p class="text-white text-lg">Coming soon ..</p>
+    </div>
+    <div
+      class="row-start-2 row-end-4 bg-chart-mint-200 rounded-lg shadow-md p-4"
+    >
+      <p class="text-white text-lg">Coming soon ..</p>
+    </div>
+    <div
+      class="col-start-1 col-end-3 bg-chart-purple-800 rounded-lg shadow-md p-4"
+    >
+      <p class="text-white text-lg">Coming soon ..</p>
+    </div>
+  </div>
 </template>
