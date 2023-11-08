2f91fc562595d0d175cf67bab01e75efdc8e8dd7
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-08 10:17:13 -0500

    🌈 style: Removing side-bar

--- app.vue
+++ app.vue
@@ -17,3 +17,23 @@ useSeoMeta({
   // twitterCard: "summary_large_image",
 });
 </script>
+
+<style>
+/* .page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+} */
+
+* {
+  scroll-behavior: smooth;
+}
+
+*::-webkit-scrollbar {
+  display: none;
+}
+</style>
