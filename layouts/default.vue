88730fc20e05b1cd4ea662d4567bd6b0d05aa605
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-08 10:35:36 -0500

    🐞 fix: Colors & Naming

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1 class="text-4xl font-bold text-center m-12 text-chart-purple-800">
+    <h1
+      class="text-4xl font-bold text-center m-12 text-secondary-main-color-800"
+    >
       Brainstorm .
     </h1>
     <Navbar />
