87d370e61c785d95812302ad39f12c39703407ae
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2023-11-09 10:42:26 -0500

    ✨ feat: Index resume
    Adding 3 sections in the index

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,40 @@
 <template class="flex items-center">
-  <h1 class="text-3xl">Index page</h1>
   <div
-    class="summary w-1/3 mx-auto grid grid-cols-2 grid-rows-4 h-screen pt-10 gap-5"
+    class="summary w-3/4 lg:w-1/3 mx-auto grid grid-cols-2 grid-rows-4 pt-10 gap-5"
   >
-    <div
+    <a
+      href="/podcast"
       class="flex flex-col gap-8 justify-end row-start-1 row-end-3 bg-chart-blue-300 rounded-lg shadow-md p-4"
     >
-      <img
-        src="../assets/svg/podcast.svg"
-        class="h-80"
-        alt="Description de l'icône SVG"
-      />
-      <p class="text-white text-3xl font-bold">Podcast récent</p>
-    </div>
+      <img src="../assets/svg/podcast.svg" class="h-80" alt="Podcast SVG" />
+      <p class="text-white lg:text-3xl text-xl font-bold">Podcast récent</p>
+    </a>
     <div class="bg-chart-pink-500 rounded-lg shadow-md p-4">
       <p class="text-white text-lg">Coming soon ..</p>
     </div>
     <div class="bg-chart-brown-400 rounded-lg shadow-md p-4">
       <p class="text-white text-lg">Coming soon ..</p>
     </div>
-    <div
+    <a
+      href="/decrypt"
       class="row-start-2 row-end-4 bg-chart-mint-200 rounded-lg shadow-md p-4"
     >
-      <p class="text-white text-lg">Coming soon ..</p>
-    </div>
+      <p class="text-white lg:text-3xl text-xl font-bold">Dernier decryptage</p>
+      <img
+        src="../assets/svg/decryptage.svg"
+        class="h-80"
+        alt="Decryptage SVG"
+      />
+    </a>
     <div
-      class="col-start-1 col-end-3 bg-chart-purple-800 rounded-lg shadow-md p-4"
+      class="col-start-1 col-end-3 bg-chart-purple-800 rounded-lg shadow-md p-4 flex flex-column w-full justify-around items-center"
     >
-      <p class="text-white text-lg">Coming soon ..</p>
+      <p class="text-white lg:text-3xl text-xl font-bold">Portraits</p>
+      <img
+        src="../assets/svg/portrait.svg"
+        class="h-60 m-1"
+        alt="Portrait SVG"
+      />
     </div>
   </div>
 </template>
